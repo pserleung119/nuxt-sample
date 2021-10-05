33a96bc34725e2c50b0644049541aa5911331b27
Chris Leung <32382197+pserleung119@users.noreply.github.com>
2021-10-05 16:35:26 +0900

    add home detail page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <div v-for="home in homes" :key="home.objectID" style="float:left;margin:10px">
-      <home-card :home="home" />
+      <nuxt-link :to="`/home/${home.objectID}`" no-prefetch>
+        <home-card :home="home" />
+      </nuxt-link>
     </div>
   </div>
 </template>
